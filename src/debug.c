cd70957fff98d808566591ba0680a9f929270918
ayb <ayb@3hg.fr>
2021-05-01 22:33:33 +0200

    Added new debugging functions.

--- src/debug.c
+++ src/debug.c
@@ -3,6 +3,26 @@
 #include "lexer.h"
 #include "syntax.h"
 
+static char *op2string(int op);
+
+static char *
+op2string(int op)
+{
+	switch (op) {
+	case O_PLUS:  return "plus";
+	case O_MINUS: return "minus";
+	case O_MULT:  return "multiplication";
+	case O_DIV:   return "division";
+	case O_NEG:   return "negation";
+	case O_EQU:   return "equal";
+	case O_GRT:   return "greater";
+	case O_GRTEQ: return "greater/equal";
+	case O_LWR:   return "lower";
+	case O_LWREQ: return "lower/equal";
+	}
+	return NULL;
+}
+
 void
 print_token(Token t)
 {
@@ -45,4 +65,73 @@ print_token(Token t)
 		return;
 	}
 	printf("\t%*s\n", t.length, t.src);
+}
+
+void
+print_type(Type t)
+{
+	switch (t.type) {
+	case T_VAR:
+		printf("{\"type variable\":\"%lc\"}", L'α' + t.tvar);
+		break;
+	case T_CON:
+		printf("{\"type constructor\":{\"name\":\"%s\",\"arguments\":[",
+		       t.name);
+		for (int i = 0; i < t.arity; ++i) {
+			if (i != 0) printf(",");
+			print_type(t.args[i]);
+		}
+		printf("]}}");
+		break;
+	case T_FUN:
+		printf("{\"function type\":{\"arguments\":[");
+		for (int i = 0; i < t.arity; ++i) {
+			if (i != 0) printf(",");
+			print_type(t.args[i]);
+		}
+		printf("],\"return\":");
+		print_type(*t.res);
+		printf("}}");
+		break;
+	}
+}
+
+void
+print_expr(Expr e)
+{
+	printf("{");
+	switch (e.type) {
+	case E_VAR:
+		printf("\"var\":\"%s\",\"bind\":[", e.name);
+		for (int i = 0; i < e.polybind.len; ++i) {
+			if (i != 0) printf(",");
+			print_type(e.polybind.args[i]);
+		}
+		printf("]");
+		break;
+	case E_NUM:
+		printf("\"integer\":%d", e.number);
+		break;
+	case E_FUNCALL:
+		printf("\"call\":{\"function\":");
+		print_expr(*e.left);
+		printf(",\"arguments\":[");
+		for (int i = 0; i < e.args.length; ++i) {
+			if (i != 0) printf(",");
+			print_expr(((Expr *)e.args.p)[i]);
+		}
+		printf("]}");
+		break;
+	case E_OP:
+		printf("\"operator\":{\"name\":\"%s\",\"lhs\":",
+		       op2string(e.op));
+		print_expr(*e.left);
+		printf(",\"rhs\":");
+		print_expr(*e.right);
+		printf("}");
+		break;
+	}
+	printf(",");
+	print_type(e.t);
+	printf("}");
 }
