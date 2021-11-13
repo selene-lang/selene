5cb6dc9bdf1c5c92b9b557b45402f1e0b7be0a48
ayb <ayb@3hg.fr>
2021-11-13 19:05:44 +0100

    Continued the compiler.

--- src/compile.c
+++ src/compile.c
@@ -3,8 +3,11 @@
 #include "chunk.h"
 #include "syntax.h"
 
+static u8 regs[128] = {0};
+
 static void add_instruction(Instruction i, Chunk *c);
 static u8 new_reg(void);
+static void free_reg(u8 reg);
 static u8 add_const_int(int n, Chunk *c);
 static u8 compile_op(Expr *lhs, Expr *rhs, int op, Chunk *c);
 static u8 compile_expr(Expr e, Chunk *c);
@@ -13,6 +16,24 @@ static void
 add_instruction(Instruction i, Chunk *c)
 {
 	array_write(&c->code, &i);
+}
+
+static u8
+new_reg(void)
+{
+	for (int i = 0; i < 128; ++i) {
+		if (regs[i] == 0) {
+			regs[i] = 1;
+			return i;
+		}
+	}
+	return -1;
+}
+
+static void
+free_reg(u8 reg)
+{
+	regs[reg] = 0;
 }
 
 static u8
@@ -34,9 +55,16 @@ compile_op(Expr *lhs, Expr *rhs, int op, Chunk *c)
 		r = compile_expr(*rhs, c);
 	switch (op) {
 	case O_PLUS:
+		i.a = o;
+		i.b = l;
+		i.c = r;
+		i.op = OP_ADDI;
+		free_reg(l);
+		free_reg(r);
 		break;
 	}
 	add_instruction(i, c);
+	return o;
 }
 
 static u8
